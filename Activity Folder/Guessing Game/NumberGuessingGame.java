2c7a747b0960267e3e72e0b3ec3b84683554be09
Elijah Prosperie <technov@Elijahs-MacBook-Pro.local>
2025-05-11 11:10:18 -0700

    made some improvements on numberguessing game but still not quite finished.

--- Activity Folder/Guessing Game/NumberGuessingGame.java
+++ Activity Folder/Guessing Game/NumberGuessingGame.java
@@ -1,22 +1,39 @@
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int min = 1;
-        int max = 100;
+        int max = 10;
         int userGuess;
+        int randomNumber;
 
-        System.out.printf("Select a number between %d-%d:", min, max);
+        Random random = new Random();
+        randomNumber = random.nextInt(min, max);
+        System.out.printf("Select a number between %d-%d: ", min, max);
         userGuess = scanner.nextInt();
 
-        if(userGuess > max) {
-            System.out.printf("Sorry! %d is too big!", userGuess);
-            
-        }
-        
+        do {
+            System.out.println("this is number" + randomNumber);
+            userGuess = scanner.nextInt();
 
-        scanner.close();
+            if (userGuess == randomNumber) {
+                System.out.println("Congrats! You win!");
+            } else if (userGuess < randomNumber) {
+                userGuess = scanner.nextInt();
+                System.out.println("Go a little higher!");
+
+            } else if (userGuess > randomNumber) {
+                userGuess = scanner.nextInt();
+
+                System.out.println("Too high! Back down");
+
+            }
+            scanner.close();
+        } while (userGuess != randomNumber);
+        System.out.println("Game over!");
+
     }
 }
