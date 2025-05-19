21eb804b8fe62aa933811b4965850220d5b4de88
Techno <elijahprosperie@gmail.com>
2025-05-19 13:45:25 -0700

    Functionality of 3 out of 4 functionalities complete. Created a while loop and placed the if statements inside for better code.

--- Activity Folder/Banking Program/BankingProgram.java
+++ Activity Folder/Banking Program/BankingProgram.java
@@ -4,31 +4,60 @@ import java.util.Scanner;
 public class BankingProgram {
 
     public static void main(String[] args) {
+        Double depositedAmount;
         Double balance = 0.0;
         Integer choice;
+        Boolean cont;
+        Boolean activeBank = true;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Welcome to your private bank app!");
-        System.out.print("\n1. Balance\n2. Deposit\n3. Withdraw\n4. Quit \n");
-        choice = scanner.nextInt();
+        while (activeBank) {
+            System.out.print("Welcome to your private bank app!");
+            System.out.print("\n1. Balance\n2. Deposit\n3. Withdraw\n4. Quit \n");
+            choice = scanner.nextInt();
 
-        if (choice == 1) {
-            System.out.printf("Your current balance is $%.2f\n", balance);
-            System.out.print("Is there anything else you would like to do?");
-            System.out.print("\n2. Deposit\n3. Withdraw\n4. Quit \n");
-        }
-        choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.printf("Your current balance is $%.2f\n", balance);
+                System.out.print("Is there anything else you would like to do?");
+                System.out.print("\n2. Deposit\n3. Withdraw\n4. Quit \n");
 
-        if (choice == 4) {
-            System.out.print("Thank you for using our private banking app. Goodbye!");
+                choice = scanner.nextInt();
+            }
+            if (choice == 2) {
+                System.out.print("How much would you like to deposit? ");
+                depositedAmount = scanner.nextDouble();
+                depositMoney(balance, depositedAmount);
+                balance = depositMoney(balance, depositedAmount);
+                System.out.printf("Your new balance is %.2f\n", balance);
+
+                System.out.println("Would you like to make another deposit? (true/false)");
+                cont = scanner.nextBoolean();
+                while (cont) {
+                    System.out.print("How much would you like to deposit? ");
+                    depositedAmount = scanner.nextDouble();
+                    depositMoney(balance, depositedAmount);
+                    balance = depositMoney(balance, depositedAmount);
+                    System.out.printf("Your new balance is %.2f\n", balance);
+
+                    System.out.println("Would you like to make another deposit? (true/false)");
+                    cont = scanner.nextBoolean();
+                }
+            }
+            if (choice == 4) {
+                System.out.print("Thank you for choosing out bank! Have a great day!");
+                activeBank = false;
+            }
+
         }
 
         scanner.close();
     }
 
-    static void depositMoney() {
+    static Double depositMoney(Double balance, Double depositedAmount) {
         //this method will hold data for depositing money into account.
+        balance += depositedAmount;
+        return balance;
     }
 
     static void withdrawMoney() {
