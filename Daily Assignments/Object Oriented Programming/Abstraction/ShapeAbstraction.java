ab77bd4124195e691ec8ff67d1fdef8efb0b9317
Techno <technov@Elijahs-MacBook-Pro.local>
2025-08-10 07:01:00 -0700

    Abstraction folder complete.

---
+++ Daily Assignments/Object Oriented Programming/Abstraction/ShapeAbstraction.java
@@ -0,0 +1,8 @@
+
+public abstract class ShapeAbstraction {
+    abstract double area(); // Abstract method
+
+    void displayShape() {// concrete method
+        System.out.println("Shape exist!");
+    }
+}
