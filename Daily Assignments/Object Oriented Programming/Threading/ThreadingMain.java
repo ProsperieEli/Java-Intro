50a77985e299bc979687034c31b5775a9f9e3a16
Techno <elijahprosperie@gmail.com>
2025-08-25 11:04:26 -0700

    Threading folder created

---
+++ Daily Assignments/Object Oriented Programming/Threading/ThreadingMain.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class ThreadingMain {
+
+    /*
+    As we come to a close on our Java journey, let's go over Threading. 
+    Threading allows a program to run multiple task simultaneously. It helps improve performance with time-consuming operations.
+    In this program, we'll create a program that ask for user input on a timed schedule. If user does not input on time, the program will close.
+     */
+    public static void main(String[] args) {
+        /*
+         * Here, we are creating our Thread class. We create our class of ThreadingRunnable and pass that in to the Thread object. We then use the setDaemon to 
+         * close the program should the main thread(main file) complete. This means we have logic that will end the program if the timer runs out or if user enters a name beforehand.
+         */
+        ThreadingRunnable threadingRunnable = new ThreadingRunnable();
+        Thread thread = new Thread(threadingRunnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("You have ten(10) seconds to input your name: ");
+        String response = scanner.nextLine();
+
+        System.out.print("Your name is " + response);
+
+        scanner.close();
+    }
+}
---
+++ Daily Assignments/Object Oriented Programming/Threading/ThreadingRunnable.java
@@ -0,0 +1,24 @@
+
+public class ThreadingRunnable implements Runnable {
+
+    /*
+     * Let's start with this file. This file is where our threading is happening. We implement the Runnable operation and override the run() method. We then 
+     * impliment our counting logic and use the System.exit() method to close the program should the logic be met.
+     */
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("There was an interruption.");
+            }
+            if (i == 10) {
+                System.out.print("Time's up!");
+                System.exit(0);
+
+            }
+        }
+    }
+
+}
